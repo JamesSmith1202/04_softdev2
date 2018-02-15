f9602820d92f8a5bbe964e0d81c310b90e76f796
JamesSmith1202 <jsmith@stuy.edu>
2018-02-14 23:16:06 -0500

    wrk :rat:

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+import pprint
 
-connection = pymongo.MongoClient('homer.stuy.edu')
+connection = MongoClient('homer.stuy.edu')
 db = connection.test
 collection = db.restaurants
 
@@ -13,15 +14,37 @@ def get_zipcode(zipcode):
 
 def get_zip_grade(zipcode, grade):
     zipcode = str(zipcode)
-    grade = str(grade).upper
+    grade = str(grade).upper()
     return collection.find({'address.zipcode': zipcode, 'grades.grade': grade})
 
 def get_zip_score(zipcode, score):
     zipcode = str(zipcode)
     score = str(score)
-    return collection.find({'address.zipcode': zipcode, 'grades.score': {$lt: score}})
+    return collection.find({'address.zipcode': zipcode, 'grades.score': { '$lt': score }})
 
-print get_borough("Brooklyn")
-print get_zipcode("11368")
-print get_zip_grade("11368", "A")
-print get_zip_score("11368", "10")
+def get_cuisine_borough(cuisine, borough):
+    return collection.find({'cuisine': cuisine, 'borough': borough})
+
+def print_restaurants(restaurants):
+    for i in restaurants:
+        pprint.pprint(i)
+
+print "get_borough(Brooklyn)..."
+print_restaurants(get_borough("Brooklyn"))
+print("-----------------------------\n")
+
+print "get_zipcode(11368)"
+print_restaurants(get_zipcode("11368"))
+print("-----------------------------\n")
+
+print "get_zip_grade(11368, A)"
+print_restaurants(get_zip_grade("11368", "A"))
+print("-----------------------------\n")
+
+print "get_zip_score(11368, 10)"
+print_restaurants(get_zip_score("11368", "10"))
+print("-----------------------------\n")
+
+print "get_cuisine_borough(Spanish, Queens)"
+print_restaurants(get_cuisine_borough("Spanish", "Queens"))
+print("-----------------------------\n")

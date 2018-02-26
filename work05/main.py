680b0dfb870ddf92feb9bdd955291636f5db7cda
JamesSmith1202 <jsmith@stuy.edu>
2018-02-25 21:16:18 -0500

    swapped databases because the other one was broken, thus gave us pain

--- work05/main.py
+++ work05/main.py
@@ -10,15 +10,25 @@ from json import load
 
 
 connection = MongoClient('homer.stuy.edu')
+collection = connection.stan_smith_xus #the database
+db = collection.movies
+
 #if the database hasnt been made, then make it and add it
 if not 'stan_smith_xus' in connection.database_names():
-    print "i tried"
-    collection = connection.stan_smith_xus #the database
-    db = collection.events
-    with open('events.json') as f:
-        event = load(f)
-        print event
-        db.insert(event)
+    counter = 0
+    f = open("movies.json", "r")
+    movies = load(f)
+    for movie in movies:
+        if(counter % 100 == 0):
+            print "Adding database...adding movie #{}".format(counter)
+        db.insert_one(movie)
+        counter += 1
+    f.close()
+'''
 else: #for debugging, the db is removed if you run the program after making a db on the server
     print "removing..."
     connection.drop_database('stan_smith_xus')
+'''
+
+
+connection.close()
